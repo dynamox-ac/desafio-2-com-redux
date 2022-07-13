e19a83242606ee8775e2e690fc8d1048e1e08d0b
luanavfg <luanavefago1@gmail.com>
2022-07-13 11:54:35 -0300

    feat: css-grid layout

--- pages/signin/index.js
+++ pages/signin/index.js
@@ -1,9 +1,11 @@
+import { CssBaseline } from "@material-ui/core";
 import React from "react";
 import SignIn from "../../components/SignIn/SignIn"
 
 const SignInPage = () => { 
    return (
       <main>
+         <CssBaseline />
          <SignIn />
       </main>
    );  
